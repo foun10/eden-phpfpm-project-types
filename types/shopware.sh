69c9012e8d2c9cd601a5af42d684159ef4fa6073
Alexander Schneider <alexanderschneider85@gmail.com>
2019-11-07 13:37:40 +0100

    Adjust shopware

--- types/shopware.sh
+++ types/shopware.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-mysql -h ${db_host} -u ${db_user} -p${db_pass} ${db_name} -e "UPDATE s_core_shops SET base_path=NULL, base_url=NULL,hosts='${project_url}',host='${project_url}', secure=0, always_secure=0 WHERE s_core_shops.default=1;"
-mysql -h ${db_host} -u ${db_user} -p${db_pass} ${db_name} -e "INSERT s_core_auth (roleID, localeID, username, name, email, encoder, password, failedlogins, active, lockeduntil) VALUES (1, 1, '${user_mail}', 'admin', '${user_mail}', 'md5', MD5('${user_pass}'), 0, 1, '0001-01-01 00:00:00');"
+mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASS} ${DB_NAME} -e "UPDATE s_core_shops SET base_path=NULL, base_url=NULL,hosts='${PROJECT_URL}',host='${PROJECT_URL}', secure=0, always_secure=0 WHERE s_core_shops.default=1;"
+mysql -h ${DB_HOST} -u ${DB_USER} -p${DB_PASS} ${DB_NAME} -e "INSERT s_core_auth (roleID, localeID, username, name, email, encoder, password, failedlogins, active, lockeduntil) VALUES (1, 1, '${USER_MAIL}', 'admin', '${USER_MAIL}', 'md5', MD5('${USER_PASS}'), 0, 1, '0001-01-01 00:00:00');"
 
 echo "create config.php"
 cat <<EOF > config.php
 <?php return array(
     'db' =>
         array(
-            'username' => '${db_user}',
-            'password' => '${db_pass}',
-            'host' => '${db_host}',
+            'username' => '${DB_USER}',
+            'password' => '${DB_PASS}',
+            'host' => '${DB_HOST}',
             'port' => '3306',
-            'dbname' => '${db_name}',
+            'dbname' => '${DB_NAME}',
         ),
     'front' => array(
         'noErrorHandler' => true,
@@ -26,19 +26,19 @@ cat <<EOF > config.php
 );
 EOF
 
-if [[ -f "${htdocs_dir}/cache/clear_cache.sh" ]]; then
+if [[ -f "${HTDOCS_DIR}/cache/clear_cache.sh" ]]; then
     echo "clearing caches!"
-    chmod 777 "${htdocs_dir}/cache/clear_cache.sh"
-    "${htdocs_dir}/cache/clear_cache.sh"
-    chmod -R 777 "${htdocs_dir}/cache"
-elif [[ -f "${htdocs_dir}/var/cache/clear_cache.sh" ]]; then
+    chmod 777 "${HTDOCS_DIR}/cache/clear_cache.sh"
+    "${HTDOCS_DIR}/cache/clear_cache.sh"
+    chmod -R 777 "${HTDOCS_DIR}/cache"
+elif [[ -f "${HTDOCS_DIR}/var/cache/clear_cache.sh" ]]; then
     echo "clearing caches!"
-    chmod 777 ${htdocs_dir}/var/cache/clear_cache.sh
-    "${htdocs_dir}/var/cache/clear_cache.sh"
-    chmod -R 777 "${htdocs_dir}/var"
+    chmod 777 ${HTDOCS_DIR}/var/cache/clear_cache.sh
+    "${HTDOCS_DIR}/var/cache/clear_cache.sh"
+    chmod -R 777 "${HTDOCS_DIR}/var"
 fi
 
-if [[ ! -f "${htdocs_dir}/index.php" ]]; then
+if [[ ! -f "${HTDOCS_DIR}/index.php" ]]; then
     echo "create index.php"
     cat <<EOF > index.php
 <?php
@@ -47,13 +47,13 @@ EOF
 fi
 
 echo "Add media dirs"
-rm -rf ${htdocs_dir}/media
-mkdir -p ${htdocs_dir}/media
-mkdir -p ${htdocs_dir}/media/archive
-mkdir -p ${htdocs_dir}/media/image
-mkdir -p ${htdocs_dir}/media/image/thumbnail
-mkdir -p ${htdocs_dir}/media/music
-mkdir -p ${htdocs_dir}/media/pdf
-mkdir -p ${htdocs_dir}/media/temp
-mkdir -p ${htdocs_dir}/media/unkown
-mkdir -p ${htdocs_dir}/media/video
+rm -rf ${HTDOCS_DIR}/media
+mkdir -p ${HTDOCS_DIR}/media
+mkdir -p ${HTDOCS_DIR}/media/archive
+mkdir -p ${HTDOCS_DIR}/media/image
+mkdir -p ${HTDOCS_DIR}/media/image/thumbnail
+mkdir -p ${HTDOCS_DIR}/media/music
+mkdir -p ${HTDOCS_DIR}/media/pdf
+mkdir -p ${HTDOCS_DIR}/media/temp
+mkdir -p ${HTDOCS_DIR}/media/unkown
+mkdir -p ${HTDOCS_DIR}/media/video
