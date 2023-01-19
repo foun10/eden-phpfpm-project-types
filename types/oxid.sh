f4f10e05a674690539a3320dfd15869dfa8e6024
Alexander Schneider <schneider@foun10.de>
2023-01-19 09:25:21 +0100

    Adjust oxid

--- types/oxid.sh
+++ types/oxid.sh
@@ -1,8 +1,28 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-if [[ ! -f "${HTDOCS_DIR}/config.local.inc.php" ]]; then
-echo "create config.local.inc.php"
-cat <<EOF > ${HTDOCS_DIR}/config.local.inc.php
+ACTION=${1}
+
+wait_for_db() {
+  echo "waiting for mysql service"
+  while ! mysqladmin ping -h"${1}" --silent; do
+      echo -ne "."\\r
+      sleep 5
+  done
+}
+
+case $ACTION in
+  CREATE_CONFIG)
+    DIR=${2}
+    DB_HOST=${3}
+    DB_NAME=${4}
+    DB_USER=${5}
+    DB_PASS=${6}
+    PROJECT_URL=${7}
+
+    if [[ ! -f "${DIR}/config.local.inc.php" ]]; then
+      echo "create config.local.inc.php"
+      cat <<EOF > "${DIR}/config.local.inc.php"
 <?php
 \$this->dbHost = '${DB_HOST}'; // database host name
 \$this->dbName = '${DB_NAME}'; // database name
@@ -11,33 +31,85 @@ cat <<EOF > ${HTDOCS_DIR}/config.local.inc.php
 \$this->sShopURL = 'https://${PROJECT_URL}'; // eShop base url, required
 \$this->sSSLShopURL = 'https://${PROJECT_URL}'; // eShop SSL url, optional
 \$this->sAdminSSLURL = 'https://${PROJECT_URL}/admin'; // eShop Admin SSL url, optional
-\$this->sShopDir = '${HTDOCS_DIR}';
-\$this->sCompileDir = '${HTDOCS_DIR}/tmp';
+\$this->sShopDir = '${DIR}';
+\$this->sCompileDir = '${DIR}/tmp';
 EOF
-fi
+    fi
+    ;;
+  CREATE_USER)
+    DB_HOST=${2}
+    DB_NAME=${3}
+    DB_USER=${4}
+    DB_PASS=${5}
+    USER_MAIL=${5}
+    USER_PASS=${5}
 
-if [[ ${USER_MAIL} != '' ]] && [[ ${USER_PASS} != '' ]]; then
+    wait_for_db "${DB_HOST}"
+
+    echo "Create user ${USER_MAIL}."
+
     create_user_sql="
-    INSERT INTO oxuser (
-        OXID,
-        OXACTIVE,
-        OXRIGHTS,
-        OXSHOPID,
-        OXUSERNAME,
-        OXPASSWORD,
-        OXPASSSALT
-    ) VALUES (
-        'devadmin',
-        1,
-        'malladmin',
-        1,
-        '${USER_MAIL}',
-        MD5('${USER_PASS}'),
-        ''
-    ) ON DUPLICATE KEY UPDATE
-        OXUSERNAME='${USER_MAIL}',
-        OXPASSWORD=MD5('${USER_PASS}'),
-        OXPASSSALT=''
-    "
+      INSERT INTO oxuser (
+          OXID,
+          OXACTIVE,
+          OXRIGHTS,
+          OXSHOPID,
+          OXUSERNAME,
+          OXPASSWORD,
+          OXPASSSALT
+      ) VALUES (
+          'devadmin',
+          1,
+          'malladmin',
+          1,
+          '${USER_MAIL}',
+          MD5('${USER_PASS}'),
+          ''
+      ) ON DUPLICATE KEY UPDATE
+          OXUSERNAME='${USER_MAIL}',
+          OXPASSWORD=MD5('${USER_PASS}'),
+          OXPASSSALT=''
+      "
     mysql -h${DB_HOST} -u${DB_USER} -p${DB_PASS} ${DB_NAME} -e "${create_user_sql}"
-fi
+
+    echo "User ${USER_MAIL} created."
+    ;;
+  CREATE_DEFAULT_DIRECTORIES)
+    DIR=${2}
+    # Create log DIR
+    LOG_DIR="${DIR}/log"
+
+    if [[ ! -d ${LOG_DIR} ]]; then
+      mkdir "${LOG_DIR}"
+    fi
+
+    # Create export DIR
+    EXPORT_DIR="${DIR}/export"
+
+    if [[ ! -d ${EXPORT_DIR} ]]; then
+      mkdir "${EXPORT_DIR}"
+    fi
+
+    # Flush temp DIR
+    rm -rf "${DIR}/tmp/*"
+
+    if [[ ! -d ${DIR}/tmp ]]; then
+      mkdir "${DIR}/tmp"
+    fi
+
+    echo "Default directories created."
+    ;;
+  FLUSH_MODULES_DB)
+    DB_HOST=${2}
+    DB_NAME=${3}
+    DB_USER=${4}
+    DB_PASS=${5}
+
+    wait_for_db "${DB_HOST}"
+
+    # Flush aModules database entry
+    echo "Flush aModules database entry."
+    mysql -h"${DB_HOST}" -u"${DB_USER}" -p"${DB_PASS}" "${DB_NAME}" -e "UPDATE oxconfig SET OXVARNAME = 'aModules_old' WHERE OXVARNAME = 'aModules';"
+    echo "Entries flushed."
+    ;;
+esac
